309bdb5a9160fb4cc807bb56c8f5e94d12c7a289
Noir <chenDSEE@163.com>
2022-09-28 21:43:29 +0800

    update benchmark for array, slice, map in size of 16

---
+++ benchmark/README.md
@@ -0,0 +1,28 @@
+# slice, array, map 的随机 access 速度对比（size = 16）
+
+从快到慢排列：array ≈ switch-case > slice >> map (基于 go1.17.7)
+- map 比其他的慢了一个数量级
+- 不嫌麻烦的话，直接用 switch case 是比较好的，因为还可以处理异常输入
+  - 显然，array 的方式代码是最简单的，而且直接增加枚举值就好了，但是没有异常输入的处理能力，一场输入直接靠 panic
+```bash
+[root@LC benchmark]# go version
+go version go1.17.7 linux/amd64
+[root@LC benchmark]# go test -v -bench='Benchmark_Access.*' -cpu=1 -benchtime=5s
+goos: linux
+goarch: amd64
+pkg: benchmark
+cpu: Intel(R) Xeon(R) Platinum 8269CY CPU @ 2.50GHz
+Benchmark_AccessStackArray_SmallSize
+Benchmark_AccessStackArray_SmallSize 	1000000000	         0.3325 ns/op
+Benchmark_AccessArray_SmallSize
+Benchmark_AccessArray_SmallSize      	1000000000	         0.3209 ns/op
+Benchmark_AccessSlice_SmallSize
+Benchmark_AccessSlice_SmallSize      	1000000000	         0.5421 ns/op
+Benchmark_AccessMap_SmallSize
+Benchmark_AccessMap_SmallSize        	719862216	         8.234 ns/op
+Benchmark_AccessSwitchCase_SmallSize
+Benchmark_AccessSwitchCase_SmallSize 	1000000000	         0.3337 ns/op
+PASS
+ok  	benchmark	8.457s
+[root@LC benchmark]# 
+```
---
+++ benchmark/perf_test.go
@@ -0,0 +1,142 @@
+package benchmark
+
+import "testing"
+
+
+/* result: array = switch-case > slice > map
+[root@LC benchmark]# go test -v -bench='Benchmark_Access.*' -cpu=1 -benchtime=5s
+goos: linux
+goarch: amd64
+pkg: benchmark
+cpu: Intel(R) Xeon(R) Platinum 8269CY CPU @ 2.50GHz
+Benchmark_AccessStackArray_SmallSize
+Benchmark_AccessStackArray_SmallSize 	1000000000	         0.3325 ns/op
+Benchmark_AccessArray_SmallSize
+Benchmark_AccessArray_SmallSize      	1000000000	         0.3209 ns/op
+Benchmark_AccessSlice_SmallSize
+Benchmark_AccessSlice_SmallSize      	1000000000	         0.5421 ns/op
+Benchmark_AccessMap_SmallSize
+Benchmark_AccessMap_SmallSize        	719862216	         8.234 ns/op
+Benchmark_AccessSwitchCase_SmallSize
+Benchmark_AccessSwitchCase_SmallSize 	1000000000	         0.3337 ns/op
+PASS
+ok  	benchmark	8.457s
+[root@LC benchmark]#
+*/
+
+const smallSize = 16
+
+// go test -v -bench='Benchmark_AccessStackArray_SmallSize' -cpu=1 -benchtime=5s
+func Benchmark_AccessStackArray_SmallSize(b *testing.B) {
+	var tmp string
+	var array [smallSize]string
+	for i := 0; i < smallSize; i++ {
+		array[i] = "demo-string"
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		tmp = array[i&0xf]
+	}
+	b.StopTimer()
+
+	_ = tmp
+}
+
+// go test -v -bench='Benchmark_AccessArray_SmallSize' -cpu=1 -benchtime=5s
+func Benchmark_AccessArray_SmallSize(b *testing.B) {
+	var tmp string
+	var array *[smallSize]string = new([smallSize]string) // slice and map also in heap
+	for i := 0; i < smallSize; i++ {
+		array[i] = "demo-string"
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		tmp = array[i&0xf]
+	}
+	b.StopTimer()
+
+	_ = tmp
+}
+
+// go test -v -bench='Benchmark_AccessSlice_SmallSize' -cpu=1 -benchtime=5s
+func Benchmark_AccessSlice_SmallSize(b *testing.B) {
+	var tmp string
+	slice := make([]string, 0, 10)
+	for i := 0; i < smallSize; i++ {
+		slice = append(slice, "demo-string")
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		tmp = slice[i&0xf]
+	}
+	b.StopTimer()
+
+	_ = tmp
+}
+
+// go test -v -bench='Benchmark_AccessMap_SmallSize' -cpu=1 -benchtime=5s
+func Benchmark_AccessMap_SmallSize(b *testing.B) {
+	var tmp string
+	Map := make(map[int]string, smallSize)
+	//Map := make(map[int]string)
+	for i := 0; i < smallSize; i++ {
+		Map[i] = "demo-string"
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		tmp = Map[i&0xf]
+	}
+	b.StopTimer()
+
+	_ = tmp
+}
+
+// go test -v -bench='Benchmark_AccessSwitchCase_SmallSize' -cpu=1 -benchtime=5s
+func Benchmark_AccessSwitchCase_SmallSize(b *testing.B) {
+	var tmp string
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		switch i & 0xf {
+		case 0:
+			tmp = "demo-string"
+		case 1:
+			tmp = "demo-string"
+		case 2:
+			tmp = "demo-string"
+		case 3:
+			tmp = "demo-string"
+		case 4:
+			tmp = "demo-string"
+		case 5:
+			tmp = "demo-string"
+		case 6:
+			tmp = "demo-string"
+		case 7:
+			tmp = "demo-string"
+		case 8:
+			tmp = "demo-string"
+		case 9:
+			tmp = "demo-string"
+		case 10:
+			tmp = "demo-string"
+		case 11:
+			tmp = "demo-string"
+		case 12:
+			tmp = "demo-string"
+		case 13:
+			tmp = "demo-string"
+		case 14:
+			tmp = "demo-string"
+		case 15:
+			tmp = "demo-string"
+		}
+	}
+	b.StopTimer()
+
+	_ = tmp
+}
